b2c280e1d964a331c749b5ff432a13e951959290
Mohammad Hekmatnejad <m_hekmatnejad@yahoo.com>
2013-10-15 02:56:25 -0700

    update + new benchmark added

---
+++ src/main/java/drools/traits/benchmarks/TraitDonBmk.java
@@ -0,0 +1,11 @@
+package drools.traits.benchmarks;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mamad
+ * Date: 10/15/13
+ * Time: 1:27 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TraitDonBmk {
+}
